b0fa1a2c844ca039d72fde8d7c921c6de3a0c199
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-03-20 22:16:36 +0530

    prisma updates

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,13 +1,31 @@
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-// Your agency page component
-const AgencyPage = () => {
-  return (
-    <div>
-      <h1>Agency Page</h1>
-      {/* Add your agency page content here */}
-    </div>
-  );
-};
+// // Your agency page component
+// const AgencyPage = () => {
+//   return (
+//     <div>
+//       <h1>Agency Page</h1>
+//       {/* Add your agency page content here */}
+//     </div>
+//   );
+// };
 
-export default AgencyPage;
+const Page = async () => {
+    const authUser = await currentUser()
+    if(!authUser) return redirect('/sign-in')
+    return <div>Agency</div>
+}
+
+export default Page
+
+// const Page = async () => {
+//     const authUser = await currentUser()
+//     if (authUser) return redirect('/sign-in')
+//     return <div>Agency</div>
+// }
+
+// export default Page;
+// export default AgencyPage;
+
