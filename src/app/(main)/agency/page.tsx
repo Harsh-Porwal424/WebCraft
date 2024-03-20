f7d7d5f47b028364070a08b0d75b24721e1f404c
Harsh Porwal <harshporwal@Harshs-MacBook-Air.local>
2024-03-20 23:19:57 +0530

    login backend

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,31 +1,20 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries';
 import { currentUser } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
-// // Your agency page component
-// const AgencyPage = () => {
-//   return (
-//     <div>
-//       <h1>Agency Page</h1>
-//       {/* Add your agency page content here */}
-//     </div>
-//   );
-// };
 
 const Page = async () => {
-    const authUser = await currentUser()
-    if(!authUser) return redirect('/sign-in')
+    // const authUser = await currentUser()
+    // if(!authUser) return redirect('/sign-in')
+
+    const agencyId = await verifyAndAcceptInvitation()
+    console.log(agencyId)
+
+    const user = await getAuthUserDetails()
     return <div>Agency</div>
 }
 
 export default Page
 
-// const Page = async () => {
-//     const authUser = await currentUser()
-//     if (authUser) return redirect('/sign-in')
-//     return <div>Agency</div>
-// }
-
-// export default Page;
-// export default AgencyPage;
 
